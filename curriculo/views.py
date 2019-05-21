db0ef969b9167e77f4535cad6652e3035d0047e5
Samuel Motta Muzelli <1801624@SALAS.AULAS>
2019-05-21 19:05:36 -0300

    Ac9

--- curriculo/views.py
+++ curriculo/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render, get_object_or_404
 
 from curriculo.models import Curso
 
+
 def curso(request, sigla):
+    c = get_object_or_404(Curso, sigla=sigla)
     context = {
-        'curso': get_object_or_404(Curso, sigla=sigla)
+        'curso': c,
+        # Aqui substitua o dicionario vazio pelo gerado pelo método
+        # monta_matriz
+        'matriz': c.monta_matriz()
     }
     return render(request, 'curriculo/curso.html', context)
